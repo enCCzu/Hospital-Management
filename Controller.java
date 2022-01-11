5522271452f23dfbca0aeae98d0e35fe1103eebc
SchoolDanDan <95246101+SchoolDanDan@users.noreply.github.com>
2022-01-11 17:08:36 -0500

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,9 @@
+import java.io.*; 
+import java.awt.*;
+import java.util.ArrayList;
+class Controller extends ControllerHelperMethods{
+
+    public Controller(){
+        Model model = new Model();
+    }
+}
---
+++ ControllerHelperMethods.java
@@ -0,0 +1,25 @@
+import java.io.*; 
+import java.awt.*;
+import java.util.ArrayList;
+abstract class ControllerHelperMethods {
+
+    public boolean checkPassword(){
+
+        return true;
+    }
+
+    public String[] getTable(String[] table){
+
+        return table;
+    }
+
+    public String[] sendTable(String[] table){
+
+        return table;
+    }
+
+    public String[] editTable(String[] table){
+
+        return table;
+    }
+}
