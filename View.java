2a69f498be0432ed973a207dc84ed417301a09b3
SchoolDanDan <95246101+SchoolDanDan@users.noreply.github.com>
2022-01-12 14:39:36 -0500

    Add files via upload

---
+++ View.java
@@ -0,0 +1,8 @@
+import javax.swing.*; 
+import java.awt.*; 
+class View extends AbstractView {
+    Room roomPanel = new Room();
+    Patient patientPanel = new Patient();
+    Sidebar sidebarPanel = new Sidebar();
+    Controller controller = new Controller();
+}
