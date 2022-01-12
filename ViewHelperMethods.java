e3d6045a1427553947d79c4b6645f37dc3cf975e
Erin Zhou <92766077+enCCzu@users.noreply.github.com>
2022-01-12 14:41:00 -0500

    Created abstract view class with empty methods

---
+++ ViewHelperMethods.java
@@ -0,0 +1,30 @@
+import javax.swing.*; 
+import java.awt.*;
+
+
+abstract class ViewHelperMethods {
+    
+    protected void refreshFrame(){
+
+
+
+    }
+
+    protected void refreshPanel(){
+
+
+
+    }
+
+    protected void deconstructPanel(){
+
+
+    }
+
+    protected void makeFrame(){
+
+
+        
+    }
+
+}
