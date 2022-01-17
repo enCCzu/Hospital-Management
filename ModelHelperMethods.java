d807cdd04bc8309c426c121f67cc8d87a1f95ddc
SchoolDanDan <95246101+SchoolDanDan@users.noreply.github.com>
2022-01-17 15:38:34 -0500

    Add files via upload

--- ModelHelperMethods.java
+++ ModelHelperMethods.java
@@ -19,16 +19,65 @@ abstract class ModelHelperMethods {
 
     }
 
-    protected void hashMapToCSV(){
+    protected void hashMapToCSV(HashMap<String,String> userLogin){
+        try{
+            // Initializing the File
+            FileWriter file = new FileWriter("UserLogin.csv");
+             
+            // Initialing BufferedWriter
+            BufferedWriter writer = new BufferedWriter(file);
 
+            // Looping through Hashmap and writing to file
+            for(String i : userLogin.keySet()) {
+                writer.write(i + "|" + userLogin.get(i));
+                writer.newLine();
+            }
+            
+            // Close file
+            writer.close();
+
+            System.out.println("Written successfully");
+        }
+        catch (IOException except)
+        {
+            except.printStackTrace();
+        }
 
     }
 
 
-    protected  void arrayListToCSV(){
+    protected  void arrayListToCSV(ArrayList<String[]> table, String tableName){
+        String line = "";
+        try{
+            // Initializing the File
+            FileWriter file = new FileWriter(tableName + "Table.csv");
+             
+            // Initialing BufferedWriter
+            BufferedWriter writer = new BufferedWriter(file);
 
+            // Looping through ArrayList 
+            for(String[] i : table) {
+                //Looping through Array and setting the line
+                for(String j : i){
+                    line = line + j + "|";
+                }
+                //Writes to the file
+                writer.write(line.substring(0, line.length()-1));
+                writer.newLine();
 
-
+                //Resets the line
+                line = "";
+            }
+            
+            // Close file
+            writer.close();
+            
+            System.out.println("Written successfully");
+        }
+        catch (IOException except)
+        {
+            except.printStackTrace();
+        }
     }
 
     protected ArrayList getTable(){
