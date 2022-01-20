f6b36ec0e4e1623b15ac6d9ad669a240dd36b787
SchoolDanDan <95246101+SchoolDanDan@users.noreply.github.com>
2022-01-20 15:21:44 -0500

    dashboard tab

---
+++ Dashboard.java
@@ -0,0 +1,31 @@
+//imports 
+
+import javax.swing.*; 
+import java.awt.*;
+
+public class Dashboard extends JPanel {
+  
+  
+  public Dashboard(){
+    
+    
+    setLayout(null); 
+    
+    setSize(900,800); 
+    
+    setBackground(Color.WHITE);
+    
+    
+    
+    JLabel title = new JLabel("Dashboard");
+    
+    add(title); 
+    
+    
+    
+  }
+  
+  
+  
+  
+}
