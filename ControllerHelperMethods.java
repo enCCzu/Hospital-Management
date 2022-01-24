37ef5932d12b58346fbba477eb485200132138cd
SchoolDanDan <95246101+SchoolDanDan@users.noreply.github.com>
2022-01-23 19:34:37 -0500

    added docstrings

--- ControllerHelperMethods.java
+++ ControllerHelperMethods.java
@@ -9,11 +9,24 @@ abstract class ControllerHelperMethods {
     protected PatientDatabase patientDatabase = new PatientDatabase();
     protected RoomManagementDatabase roomDatabase = new RoomManagementDatabase();
 
+    /**
+     * Checks the username and password entered by the user
+     * Returns true if the username and password match the database
+     * @param userName Username entered by user
+     * @param password Password entered by user
+     * @return Boolean of if the username and password are correct
+     */
     public boolean checkPassword(String userName, String password) {
 
         return checkPassword(userName, password);
     }
 
+    /**
+     * Gets the table from the model
+     * Gets either the patient or room table depending on requested table
+     * @param requestedTable The table that the program wants to use
+     * @return Return the table the user wants
+      */
     public ArrayList<ArrayList<String>> getTable(String requestedTable) {
 
         if (requestedTable.equals("patient")){
@@ -29,15 +42,29 @@ abstract class ControllerHelperMethods {
         return table;
     }
 
+    /**
+     * Sends the table to from view to model
+     * @param viewTable table from view
+     * @return ArrayList of the table to the model
+     */
     public ArrayList<ArrayList<String>> sendTable(ArrayList<ArrayList<String>> viewTable) {
 
         table = viewTable;
         return table;
     }
+
+    /**
+     * Finds the number of patients in the database
+     * @return returns the number of patients
+     */
     protected int findNumberOfPatients(){
         return patientDatabase.getTable().size();
     }
 
+    /**
+     * Finds the total number of beds in the database
+     * @return Returns the total number of beds
+     */
     protected int findTotalBeds(){
         int totalBeds = 0;
         ArrayList<ArrayList<String>> roomInfo = roomDatabase.getTable();
@@ -48,6 +75,10 @@ abstract class ControllerHelperMethods {
         return totalBeds;
     }
 
+    /**
+     * Finds the number of available beds
+     * @return returns the number of available beds
+     */
     protected int findAvailableBeds(){
         int availableBeds = 0;
         ArrayList<ArrayList<String>> roomInfo = roomDatabase.getTable();
@@ -57,7 +88,10 @@ abstract class ControllerHelperMethods {
 
         return availableBeds;
     }
-
+    /**
+     * Finds the number of rooms
+     * @return returns the number of rooms
+     */
     protected int findNumberOfRooms(){
         return roomDatabase.getTable().size();
     }
